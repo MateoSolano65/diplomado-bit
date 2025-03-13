c8415118b39ef1fdbc24d7c2968d6dd45f96c4a0
Jesús Solano <115440962+MateoSolano65@users.noreply.github.com>
2025-03-13 15:00:27 -0500

    📝 (main.js): add data structures, including variables, objects, and arrays, to enhance code organization and readability

--- js/js/main.js
+++ js/js/main.js
@@ -1,1 +1,57 @@
 // Archivo de JavaScript
+
+// ! Estructuras de datos
+
+// * Variables
+let nombre = "Juan";
+let apellido = "Perez";
+let edad = 25;
+let peso = 75.5;
+let altura = 1.75;
+let casado = false;
+let hijos = 0;
+
+
+// * Objetos
+let persona = {
+  nombre: "Juan",
+  apellido: "Perez",
+  edad: 25,
+  peso: 75.5,
+  altura: 1.75,
+  casado: false,
+  hijos: 0
+};
+
+// * Arreglos
+let arreglo = [ 1, 2, 3, 4, 5 ];
+
+// * Arreglos de objetos
+let personas = [
+  {
+    nombre: "Juan",
+    apellido: "Perez",
+    edad: 25,
+    peso: 75.5,
+    altura: 1.75,
+    casado: false,
+    hijos: 0
+  },
+  {
+    nombre: "Maria",
+    apellido: "Lopez",
+    edad: 30,
+    peso: 60.5,
+    altura: 1.65,
+    casado: true,
+    hijos: 2
+  }
+];
+
+
+// ! Funciones
+
+function saludar() {
+  console.log( "Hola mundo" );
+}
+
