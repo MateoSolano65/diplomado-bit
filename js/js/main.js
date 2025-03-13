c4fe899ddc7d95af1a1459ff7d40ba2b997eb13d
Jesús Solano <115440962+MateoSolano65@users.noreply.github.com>
2025-03-13 17:02:37 -0500

    ✨ (main.js): add conditional statements, loops, and functions examples to enhance learning resources

--- js/js/main.js
+++ js/js/main.js
@@ -49,9 +49,95 @@ let personas = [
 ];
 
 
+
+
+// ! Condicionales
+
+let numero = 18;
+
+if ( numero >= 18 ) {
+  console.log( "Es mayor de edad" );
+}
+else {
+  console.log( "Es menor de edad" );
+}
+
+numero = 1;
+
+switch ( numero ) {
+  case 1:
+    console.log( "Es uno" );
+    break;
+
+  case 2:
+    console.log( "Es dos" );
+    break;
+
+  default:
+    break;
+}
+
+
+// ! Ciclos y bucles
+var array = [ 1, 2, 3, 4, 5 ];
+
+for ( let numero = 0; numero < array.length; numero++ ) {
+  const element = array[ numero ];
+
+  console.log( "Ciclo for", element );
+}
+
+
+
+numero = 0;
+
+while ( numero < array.length ) {
+
+  console.log( "while", array[ numero ] );
+
+  numero++;
+}
+
+numero = 4;
+
+do {
+  console.log( "do while", array[ numero ] );
+
+  numero++;
+} while ( numero < array.length );
+
+
 // ! Funciones
 
-function saludar() {
-  console.log( "Hola mundo" );
+function sumar( a, b ) {
+  return a + b;
 }
+
+console.log( "SUMA", sumar( 5, 3 ) );
+
+
+const saludar = ( nombre ) => {
+  return "Hola " + nombre;
+};
+
+console.log( saludar( "Yisus" ) );
+
+
+
+// ! Funciones de objetos
+
+let persona2 = {
+  nombre: "Juan",
+  apellido: "Perez",
+  edad: 25,
+  peso: 75.5,
+  altura: 1.75,
+  casado: false,
+  hijos: 0,
+  saludar: function () {
+    return "Hola, soy " + this.nombre + " " + this.apellido;
+  }
+};
+
+console.log( persona2.saludar() );
 
