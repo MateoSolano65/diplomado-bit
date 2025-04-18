6fab142d8a2722586052bdd6f4838312e0844afe
Mateo Solano <115440962+MateoSolano65@users.noreply.github.com>
2025-04-18 12:44:28 -0500

    ✨ (index.ts): añadir funciones de saludo y sumar con ejemplos de uso y comentarios explicativos

--- angular/src/index.ts
+++ angular/src/index.ts
@@ -106,3 +106,49 @@ let dias: string[] = [
 console.log(dias[0]); // imprime Lunes
 console.log(dias[1]); // imprime Martes
 // console.log(dias.Lunes); // ! no se puede acceder a la propiedad Lunes de un array
+
+console.log('------------------- FUNCIONES -------------------');
+
+// ! Funciones
+// * Funciones sin parametros y sin retorno
+function saludarMundo(): void {
+  console.log('Hola mundo');
+} // no recibe parametros y no retorna nada
+saludarMundo(); // llama a la funcion saludarMundo
+
+// * Funciones con parametros y sin retorno
+function saludarPersona(nombre: string): void {
+  console.log(`Hola ${nombre}`);
+} // recibe un parametro de tipo string y no retorna nada
+saludarPersona('Juan'); // llama a la funcion saludarPersona con el parametro Juan
+
+// * Funciones con parametros y con retorno
+function sumar(a: number, b: number): number {
+  return a + b; // recibe dos parametros de tipo number y retorna un valor de tipo number
+} // suma los dos numeros y retorna el resultado
+console.log(sumar(5, 10)); // llama a la funcion sumar con los parametros 5 y 10 y imprime el resultado en la consola
+
+// * Funciones con parametros opcionales y con retorno
+function saludarPersonaOpcional(
+  nombre: string,
+  edad?: number
+): string | undefined {
+  if (edad) {
+    return `Hola ${nombre}, tienes ${edad} años`;
+  } else {
+    return `Hola ${nombre}, sin edad`;
+  } // recibe un parametro de tipo string y un parametro opcional de tipo number y retorna un valor de tipo string o undefined
+} // saluda a la persona con su nombre y edad si se proporciona la edad
+
+console.log(saludarPersonaOpcional('Juan', 25)); // llama a la funcion saludarPersonaOpcional con los parametros Juan y 25 y imprime el resultado en la consola
+console.log(saludarPersonaOpcional('Juan')); // llama a la funcion saludarPersonaOpcional con el parametro Juan y imprime el resultado en la consola
+
+// * Funciones con parametros por defecto y con retorno
+function saludarPersonaPorDefecto(
+  nombre: string,
+  edad: number = 18
+): string | undefined {
+  return `Hola ${nombre}, tienes ${edad} años`;
+} // recibe un parametro de tipo string y un parametro de tipo number con valor por defecto 18 y retorna un valor de tipo string o undefined
+
+console.log(saludarPersonaPorDefecto('Juan')); // llama a la funcion saludarPersonaPorDefecto con los parametros Juan y imprime el resultado en la consola
