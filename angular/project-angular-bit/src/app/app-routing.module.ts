8165d4c25a77208c7c0017f39f8105c8b06a4865
Mateo Solano <115440962+MateoSolano65@users.noreply.github.com>
2025-04-19 02:17:37 -0500

    ✨ (routing): update routing configuration in AppRoutingModule; add routes for home, login, hijo, and saludo components

--- angular/project-angular-bit/src/app/app-routing.module.ts
+++ angular/project-angular-bit/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { HijoComponent } from './components/hijo/hijo.component';
+import { SaludoComponent } from './components/saludo/saludo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component:AppComponent, pathMatch: 'full' },
+  {path: 'login', component:LoginComponent, pathMatch: 'full'},
+  {path: 'hijo', component:HijoComponent, pathMatch: 'full'},
+  {path: 'saludo', component:SaludoComponent, pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
