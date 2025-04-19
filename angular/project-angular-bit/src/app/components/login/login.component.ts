4edae8ae2b42f1a37ea74d79b37ec3516bf8ab73
Mateo Solano <115440962+MateoSolano65@users.noreply.github.com>
2025-04-19 02:22:48 -0500

    ✨ (login): fix styleUrl typo and improve login method logging; ensure user credentials are saved in localStorage and sessionStorage

--- angular/project-angular-bit/src/app/components/login/login.component.ts
+++ angular/project-angular-bit/src/app/components/login/login.component.ts
@@ -4,10 +4,9 @@ import { Component } from '@angular/core';
   selector: 'app-login',
   standalone: false,
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
-
   email: string = '';
   password: string = '';
 
@@ -16,18 +15,33 @@ export class LoginComponent {
   login() {
     console.log('User:', this.email);
     console.log('Password:', this.password);
+
+    localStorage.setItem('email', this.email);
+    localStorage.setItem('password', this.password);
+    console.log(
+      'User and password saved in localStorage:',
+      localStorage.getItem('email'),
+      localStorage.getItem('password')
+    );
+
+    // ! Sesion Storage
+    sessionStorage.setItem('email', this.email);
+    sessionStorage.setItem('password', this.password);
+    console.log(
+      'User and password saved in sessionStorage:',
+      sessionStorage.getItem('email'),
+      sessionStorage.getItem('password')
+    );
   }
 
   reset() {
     this.email = '';
     this.password = '';
     console.log('Form reset');
-    
   }
   onSubmit() {
     console.log('Form submitted');
     this.login();
     this.reset();
   }
-  
 }
